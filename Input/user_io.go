eafa043bba0022531de9d20b67542beda5f6325c
AkshitR1 <120027337+AkshitR1@users.noreply.github.com>
2024-06-18 16:09:24 +0530

    Operators and user input
    Operators and user input programs

---
+++ Input/user_io.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int
+	var message string
+	var Name string
+	fmt.Print("Enter your Name: ")
+	fmt.Scan(&Name)
+	fmt.Printf("Enter your Age: ")
+	fmt.Scan(&age)
+	fmt.Print("Enter your Message: ")
+	fmt.Scan(&message)
+
+}
---
+++ operator/operate.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	var a int = 10
+	b := 22
+
+	c_mod := b % a
+
+	println("Modulus is :", c_mod)
+}
