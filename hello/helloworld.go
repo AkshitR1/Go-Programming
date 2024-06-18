bff66c87f67fe21928ad6c3ef227fe471c309731
AkshitR1 <120027337+AkshitR1@users.noreply.github.com>
2024-06-18 16:07:48 +0530

    Go- Hello
    Hello world and variables in Go

---
+++ hello/helloworld.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var number int
+	number = 10
+	fmt.Println("Value of number is:", number)
+	fmt.Println("Hello World!")
+
+	var number2 = 20
+	fmt.Println(number2)
+
+	number = 30
+	fmt.Println(number)
+
+	var name, age = "Rahul", 21
+
+	fmt.Printf("The name is %s, and his age is %d ", name, age)
+}
