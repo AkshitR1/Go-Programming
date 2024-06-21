b896dcbd599eac0787bf39a4f155e1325c0af88a
AkshitR1 <120027337+AkshitR1@users.noreply.github.com>
2024-06-21 16:54:52 +0530

    switch and multi-switch cases

--- switch/switch.go
+++ switch/switch.go
@@ -2,26 +2,42 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	var day int
+// 	fmt.Println("Enter the day of week(1 to n):")
+// 	fmt.Scan(&day)
+// 	switch day {
+// 	case 1:
+// 		fmt.Println("Monday")
+// 	case 2:
+// 		fmt.Println("Tuesday")
+// 	case 3:
+// 		fmt.Println("Wednesday")
+// 	case 4:
+// 		fmt.Println("Thursday")
+// 	case 5:
+// 		fmt.Println("Friday")
+// 	case 6:
+// 		fmt.Println("Saturday")
+// 	case 7:
+// 		fmt.Println("Sunday")
+// 	default:
+// 		fmt.Println("Not a weekday")
+// 	}
+// }
+
 func main() {
 	var day int
-	fmt.Println("Enter the day of week(1 to n):")
+	fmt.Println("Enter the day of week(1 to 7):")
 	fmt.Scan(&day)
 	switch day {
-	case 1:
-		fmt.Println("Monday")
-	case 2:
-		fmt.Println("Tuesday")
-	case 3:
-		fmt.Println("Wednesday")
-	case 4:
-		fmt.Println("Thursday")
-	case 5:
-		fmt.Println("Friday")
-	case 6:
-		fmt.Println("Saturday")
-	case 7:
-		fmt.Println("Sunday")
+	case 1, 3, 5:
+		fmt.Println("Odd Weekday")
+	case 2, 4:
+		fmt.Println("Even Weekday")
+	case 6, 7:
+		fmt.Println("Weekend")
 	default:
-		fmt.Println("Not a weekday")
+		fmt.Println("Enter valid number")
 	}
 }
